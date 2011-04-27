09eff58455d8118ed75fb5472f1476836fc926b6
sunchess <sun@sun-laptop.(none)>
2011-04-27 17:05:52 +0400

    delete test strings

--- init.rb
+++ init.rb
@@ -1,6 +1,1 @@
 require File.dirname(__FILE__) + '/lib/yandex_geocode.rb'
-
-geo = Geocode.new("AKaGj00BAAAAz2OPKAIA608dLocZlaNxLvYZYsVUM57Ip9MAAAAAAAAAAAAN3VjKAA2y2tQtrqARB1EU80UO3w==")
-geo.get("Москва")
-geo.parse
-p geo.result
